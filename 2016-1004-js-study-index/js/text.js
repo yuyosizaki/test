ffa6a721e5b430b81f4f12304d69727639e2c9f5
吉崎悠 <yoshizaki@yoshizaki-no-MacBook-Pro.local>
2016-10-04 22:43:39 +0900

    [fix] 不要ファイルの削除

--- 2016-1004-js-study-index/js/text.js
+++ 2016-1004-js-study-index/js/text.js
@@ -1,10 +1,8 @@
 $(function(){
-
     //　リストのインデックス番号を取得し、.boxのインデックス番号を取得。
     //  取得したboxをjQueryからcssで赤くする。
     $(".list").click(function(){
         var index = $(".list").index(this);
         $(".box").eq(index).css("background","#933");
     })
-
 })// endfunction
