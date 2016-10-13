47bb53e192d702eef01378ae808e4ce1ebf434ba
yoshizaki <yoshizaki@yoshizaki-no-MacBook-Pro.local>
2016-10-14 01:25:50 +0900

    [fix] ファイル削除

--- 2016-1004-js-study-index/js/text.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    //　リストのインデックス番号を取得し、.boxのインデックス番号を取得。
-    //  取得したboxをjQueryからcssで赤くする。
-    $(".list").click(function(){
-        var index = $(".list").index(this);
-        $(".box").eq(index).css("background","#933");
-    })
-})// endfunction
