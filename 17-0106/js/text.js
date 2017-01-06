cba5084be06cb20afceec500625751775c3ffa83
yoshizaki <yoshizaki@yoshizaki-no-MacBook-Pro.local>
2017-01-06 23:09:14 +0900

    [add] js練習ファイル追加

--- 17-0106/js/text.js
+++ 17-0106/js/text.js
@@ -2,17 +2,6 @@ $(function(){
 
     // 参考サイト　https://app.codegrid.net/entry/practical-jquer　y-1
 
-    var i;
-    var $button = $("button");
-
-    for(i=0; i<3; i++){
-        $button.eq(i).on('click', {value: i+1},onClick);
-    }
-
-    function onClick(e){
-        alert(e.data.value);
-    }
-
     /* array配列から要素を全て取り出す。
     var array = ["a","b","c","d","e",];
     for(n=0; n<array.length; n++){
@@ -26,16 +15,34 @@ $(function(){
     var $increment = $(".increment");
 
     $increment.on("click",function(){
-        $("ul").append($('<li>あとから追加した</li>'));
+        $("ul").append($('<li>あとから追加されました。</li>'));
     })
 
-    // 赤くなるイベント
+    /* 赤くなるイベント
 
     $("ul").on("click","li",function(){
         $(this).css({
             "cursor": "pointer",
             "color": "#990",
         });
-    });
+    });*/
+
+    // hoverイベント
+
+    $("ul").on({
+        "mouseenter": function(){
+            $(this).css("color","#b00");
+        },
+        "mouseleave": function(){
+            $(this).css("color","#000");
+        }
+    },"li");
+
+    // ボタンイベント
+
+    var $click = $(".click");
+    $click.on('click',function(){
+        window.alert("クリックされました。");
+    })
 
 })
